600794e5f439d9c4500bd5e7b05dd509408658f8
Eden Aran <edenaran@gmail.com>
2021-08-09 19:04:28 +0300

    added some style

--- resources/views/helloLaravel.blade.php
+++ resources/views/helloLaravel.blade.php
@@ -8,6 +8,11 @@
 </head>
 
 <style>
+    body{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        margin: 0;
+        text-align: center;
+    }
     table {
         margin: 0 auto;
         width: fit-content;
@@ -17,13 +22,13 @@
 
     .cell {
         border: 1px solid black;
-        text-align: center;
         padding: 5px 15px;
         font-size: 1.2rem;
     }
 </style>
 
 <body>
+    <h1>Hello Laravel</h1>
     <table>
         <thead>
             <tr>
